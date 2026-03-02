750af7428f97b0a044a031bc720bb9cff8cc2bc6
Lorenzo Medina <Lorenzomedina821@gmail.com>
2026-03-02 11:42:21 -0300

    Comienzo importando las librerias a utilizar

---
+++ laberinto.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Esto permite que ingresen y salgan los datos
+#include <vector> // Esto permite utilizar los vectores dinamicos o "matrices"
+#include <stack> // Se utiliza en el algoritmo de generacion (backtracking/dfs)
+#include <queue> // Se utiliza en el algoritmo de resolucion del laberinto (bfs) "Primero en entrar primero en salir "
+#include <ctime> // Permite acceder al relog del sistema 
+#include <cstdlib> // Contiene la funciones de aletoriedad lo utizo para generar numeros aleatorios
+
+    
